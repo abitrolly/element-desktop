11ca647e92794dc746f935ce3c9328408c9bdd7b
Anatoli Babenia <anatoli@rainforce.org>
2021-01-21 23:46:22 +0300

    Script to prepare for snap packaging

---
+++ prepare.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/bash
+
+set -x
+
+echo "--- Installing yarn ---"
+apt update && apt -y upgrade && apt install -y curl gnupg
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
+apt update
+# skip interactive promts for tzdata dependency
+DEBIAN_FRONTEND=noninteractive apt-get -y install yarn
+
+echo "--- Installing dependencies ---"
+yarn install
+
+echo "--- Getting element-web from archive ---"
+yarn run fetch --importkey
+yarn run fetch --cfgdir ''
+
