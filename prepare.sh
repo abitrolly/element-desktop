ecd42d75a4043dd307d6db79f1e51aa1c08a04dc
Anatoli Babenia <anatoli@rainforce.org>
2021-01-22 01:11:56 +0300

    Install `binutils` to provide `ar` needed to create `deb`

--- prepare.sh
+++ prepare.sh
@@ -3,7 +3,7 @@
 set -x
 
 echo "--- Installing yarn ---"
-apt update && apt -y upgrade && apt install -y curl gnupg
+apt update && apt -y upgrade && apt install -y curl gnupg binutils
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
 apt update
